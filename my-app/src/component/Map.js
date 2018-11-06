6c94b15636cd80150e67f3378359e4fe535de51d
Dakota <dakotadunn84@gmail.com>
2018-11-05 19:50:40 -0800

    Add map markers

--- my-app/src/component/Map.js
+++ my-app/src/component/Map.js
@@ -1,21 +1,28 @@
 import React, {Component} from "react";
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 
-const MyMapComponent = withScriptjs(withGoogleMap((props) =>
+const MyMapComponent = withScriptjs(withGoogleMap(props => (
   <GoogleMap
     defaultZoom={8}
+    zoom={props.zoom} 
     defaultCenter={{ lat: 32.736956, lng: -117.148605  }}
+    center={props.center}
   >
-    {props.isMarkerShown && <Marker position={{ lat: 32.736956, lng: -117.148605 }} />}
+    {props.markers && 
+        props.markers
+        .filter(marker => marker.isVisible)
+        .map((marker, idx) => (
+            <Marker key={idx} position={{ lat: marker.lat, lng: marker.lng}} />
+    ))}
   </GoogleMap>
 ))
-
+);
 
 export default class Map extends Component {
     render() {
         return(
             <MyMapComponent
-                isMarkerShown
+                {...this.props}
                 googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyCg0xeDjQtEyAq9jyFVWuEVQYd7AXOt_kE"
                 loadingElement={<div style={{ height: `100%` }} />}
                 containerElement={<div style={{ height: `400px` }} />}
